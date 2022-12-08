fd57374f911c53715a57b6f005b3c2279a6c60ea
‘tahaha27’ <M.TahaMahmood27@gmail.com>
2022-12-08 23:55:40 +0500

    another commit

--- src/Landing_page.js
+++ src/Landing_page.js
@@ -1,9 +1,12 @@
+import React from "react";
+import Animation from "./Animation";
+
 const Landing_page = () => {
   return (
-    <div className="bg-background_2 ">
-      <div className="relative overflow-hidden ">
+    <div className="bg-background_2 scrollbar scrollbar-thumb-black scrollbar-track-white">
+      <div className="relative overflow-hidden">
         <div className="absolute blur-3xl opacity-[30%] bg-gradient-to-tr from-[#1F34E7] to-[#1FC3E7] bg-gradient-to-r -top-[250px] -right-[250px] w-[700px] h-[700px] rounded-full"></div>
-        <div className="absolute blur-3xl opacity-[30%] bg-gradient-to-tr from-[#1F34E7] to-[#1FC3E7] bg-gradient-to-r top-[500px] left-[350px] w-[200px] h-[200px] rounded-full"></div>
+        <div className="absolute blur-3xl opacity-[30%] bg-gradient-to-tr from-[#1F34E7] to-[#1FC3E7] bg-gradient-to-r top-[800px] left-[350px] w-[200px] h-[200px] rounded-full"></div>
         <div className="absolute blur-3xl opacity-[30%] bg-gradient-to-tr from-[#1F34E7] to-[#1FC3E7] bg-gradient-to-r top-[400px] -left-[100px] w-[300px] h-[300px] rounded-full"></div>
 
         <div className="header h-10 text-white flex justify-center">
@@ -20,8 +23,8 @@ const Landing_page = () => {
         </div>
 
         <div className="Main_Section grid grid-cols-2 pl-3">
-          <div className="Left_Part">
-            <div className="pt-[200px] text-[#E2E6FA] text-8xl">
+          <div className="Left_Part pl-3">
+            <div className="pt-[250px] text-[#E2E6FA] text-8xl">
               Build By Block
             </div>
             <div className="text-3xl text-[#E4EFFC]">
@@ -33,33 +36,39 @@ const Landing_page = () => {
               the week.
             </div>
             <div className="button text-white">
-              <button className="border-2 border-txt_1 rounded-xl pt-1 p-2 text-3xl">
+              <button className="border-2 border-white rounded-xl pt-1 p-2 text-3xl">
                 Sign Up
               </button>
-              <button className="border-2 border-txt_1 mx-1 rounded-xl pt-1 p-2 text-3xl">
+              <button className="border-2 border-white mx-1 rounded-xl pt-1 p-2 text-3xl">
                 Sign In
               </button>
-              <button className="border-2 border-txt_1 rounded-xl pt-1 p-2 text-3xl">
+              <button className="border-2 border-white rounded-xl pt-1 p-2 text-3xl">
                 Vision
               </button>
             </div>
           </div>
-          <div className="Right_Part">tdsasdaasdaja</div>
+          <div className="Right_Part">
+            <div className="relative w-[700px] -top-[30px]">
+              <Animation />
+            </div>
+          </div>
         </div>
-        <div className="kuch bhi">
-          <div className="outer_rectangle ml-[20%] w-[60%] text-md  bg-[#3E3C3C]/[10%] text-white mt-20 rounded-xl">
-            <div className="inner_rectangle pt-5 pb-5 flex justify-between ml-[4%] mr-[4%]">
+        <div className="Rectangle">
+          <div className="outer_rectangle ml-[15%] w-[70%] text-sm  bg-[#3E3C3C]/[10%] text-white mt-[30px] rounded-xl">
+            <div className="inner_rectangle pt-7 pb-7 flex justify-between ml-[8%] mr-[8%]">
               <div className="individual_rectangle w-[20%] bg-[#3E3C3C] rounded-xl ">
-                <div className="pt-6 pb-5 flex justify-center">
-                  <img src="blockchain-icon.svg" className="w-auto h-12"></img>
+                <div className="pt-6 pb-2 flex justify-center">
+                  <img src="blockchain-icon.svg" className="w-[80%] h-12"></img>
                 </div>
-                <div className="flex justify-center">
-                  <div>First BLOCKCHAIN WEBSITE FOR REALSTATE</div>
+                <div>
+                  <div className="flex text-center pb-8">
+                    SMART CONTRACT FOR REALSTATE
+                  </div>
                 </div>
               </div>
 
               <div className="individual_rectangle w-[20%] bg-[#3E3C3C] rounded-xl ">
-                <div className="pt-2 pb-2 flex justify-center">
+                <div className="pt-6 pb-2 flex justify-center">
                   <img src="24_hrs.svg" className="w-[80%] h-12"></img>
                 </div>
                 <div className="flex justify-center">
@@ -68,26 +77,25 @@ const Landing_page = () => {
               </div>
 
               <div className="individual_rectangle w-[20%] bg-[#3E3C3C] rounded-xl ">
-                <div className="pt-2 pb-2 flex justify-center">
+                <div className="pt-6 pb-2 flex justify-center">
                   <img
                     src="decentralized.svg"
                     className="w-[80%] h-12 flex justify-center"
                   ></img>
                 </div>
-                <div className="flex justify-center">
-                  <div>DECENTRALIZED NETWORK</div>
-                </div>
+
+                <div className="flex text-center">DECENTRALIZED NETWORK</div>
               </div>
 
               <div className="individual_rectangle w-[20%] bg-[#3E3C3C] rounded-xl ">
-                <div className="pt-2 pb-2 flex justify-center">
+                <div className="pt-6 pb-2 flex justify-center">
                   <img
                     src="blog-icon.svg"
                     className="w-[80%] h-12 flex justify-center"
                   ></img>
                 </div>
                 <div className="flex justify-center">
-                  <div>BLOGGING</div>
+                  <div className="flex">BLOGGING</div>
                 </div>
               </div>
             </div>
