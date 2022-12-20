c6ea260e14e2624c6388ef11fcf2aeb42273c8c5
‘tahaha27’ <M.TahaMahmood27@gmail.com>
2022-12-20 18:35:12 +0500

    another commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
+import Blog_page from "./Blog_page";
 import Landing_page from "./Landing_page";
 import Registration_Page from "./Registration_Page";
 function App() {
   return (
     <div>
-      {/* <Landing_page /> */}
-      <Registration_Page />
+      <Landing_page />
+      {/* <Registration_Page /> */}
+      {/* <Blog_page /> */}
     </div>
   );
 }
