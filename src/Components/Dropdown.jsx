9e5cd98e96541cf8f4f6b6fea8cd4ca80a513f17
‘tahaha27’ <M.TahaMahmood27@gmail.com>
2023-01-21 15:34:12 +0500

    drop-down added

--- src/Components/Dropdown.jsx
+++ src/Components/Dropdown.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Dropdown = () => {
   return (
-    <div className="absolute bg-[#E6E7ED] rounded-md	shadow-lg shadow-white	">
+    <div className="absolute bg-[#E6E7ED] rounded-md	shadow-lg shadow-white	top-6">
       <div className="shadow-lg shadow-white ">Signout</div>
       <div className="">Sign In</div>
       <div className="">My Account</div>
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,17 +1,15 @@
 import React from "react";
+import { useState } from "react";
 import Dropdown from "./Dropdown";
 const Header = () => {
-  var x = false;
+  const [showdropdown, setshowdropdown] = useState(false);
   const preventhanlder = (event) => {
-    event.preventDefault();
-    if (x === false) {
-      x = true;
-    } else {
-      x = false;
-      return x;
+    setshowdropdown(true);
+    if (showdropdown === true) {
+      setshowdropdown(false);
     }
   };
-  console.log(x);
+
   return (
     <div className="sticky top-0 z-10">
       <div className="  w-[100%] z-10 bg-white grid grid-cols-10 h-[50px] ">
@@ -20,10 +18,10 @@ const Header = () => {
           <div className="text-xs mt-1 ml-2">Hi Taha</div>
           <div className="text-md ml-2">Welcome Back,</div>
         </div>
-        <div className="Dropdown  col-span-3 relative">
-          <div className="">
+        <div className="Dropdown col-span-3 relative">
+          <div className="flex justify-end">
             <button onClick={preventhanlder}>Logo</button>
-            {x && <Dropdown />}
+            {showdropdown && <Dropdown />}
           </div>
         </div>
       </div>
