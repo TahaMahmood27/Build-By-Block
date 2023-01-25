94879b302a91caa8c92fbf2cdc002d19e879a3a7
‘tahaha27’ <M.TahaMahmood27@gmail.com>
2023-01-26 01:50:09 +0500

    some routing added

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import BlogLandingPage from "./BlogLandingPage";
 import Landing_page from "./Landing_page";
 import Registration_Page from "./Registration_Page";
 import BlogPage from "./BlogPage";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      {/* <Landing_page /> */}
-      {/* <Registration_Page /> */}
-      <BlogLandingPage />
-      {/* <BlogPage /> */}
+      <Routes>
+        <Route path="/" element={<Landing_page />}></Route>
+        <Route path="/Account_Page" element={<Registration_Page />}></Route>
+        <Route path="/Home" element={<BlogLandingPage />}></Route>
+        <Route path="/BlogPage" element={<BlogPage />}></Route>
+      </Routes>
     </div>
   );
 }
