506fffaad68c48407fa753aa165e74ba4ad2b831
‘tahaha27’ <M.TahaMahmood27@gmail.com>
2023-01-29 23:26:21 +0500

    little changes

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -13,14 +13,18 @@ const Header = () => {
   return (
     <div className="sticky top-0 z-10">
       <div className="  w-[100%] z-10 bg-white grid grid-cols-10 h-[50px] ">
-        <div className="logo col-span-2 pl-4">logo</div>
+        <div className="logo col-span-2 pl-4">
+          <img src="./Logo.png" className="h-[50px]"></img>
+        </div>
         <div className="Name-Column col-span-5 border-x-[0.1rem] ">
           <div className="text-xs mt-1 ml-2">Hi Taha</div>
           <div className="text-md ml-2">Welcome Back,</div>
         </div>
         <div className="Dropdown col-span-3 relative">
           <div className="flex justify-end">
-            <button onClick={preventhanlder}>Logo</button>
+            <button onClick={preventhanlder}>
+              <img src="./arrow.svg" className="h-8"></img>
+            </button>
             {showdropdown && <Dropdown />}
           </div>
         </div>
