2b58af0a2af52fbcbc80dbe11b8bf75b8fe26848
‘tahaha27’ <M.TahaMahmood27@gmail.com>
2023-01-29 22:05:01 +0500

    some authentication code added

--- src/Registration_Page.jsx
+++ src/Registration_Page.jsx
@@ -1,17 +1,29 @@
-import React from "react";
+import React, { useRef } from "react";
 import { useState } from "react";
-import Login from "./Components/Login";
 import Sign_up from "./Components/Sign_up";
 
 const Registration_Page = () => {
   const [login_state, setlogin_state] = useState(true);
+  const usernameRef = useRef();
+  const passwordRef = useRef();
 
-  const change_login_state = (event) => {
+  let change_login_state = async (event) => {
     event.preventDefault();
-    if (login_state === true) {
-      setlogin_state(false);
-    } else if (login_state === false) {
-      setlogin_state(true);
+    let response = await fetch("http://127.0.0.1:8000/api/token/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: usernameRef.current.value,
+        password: passwordRef.current.value,
+      }),
+    });
+    let data = await response.json();
+    if (response.status === 200) {
+      console.log(data.access);
+    } else {
+      console.log("Taha");
     }
   };
   return (
@@ -28,47 +40,77 @@ const Registration_Page = () => {
           <div className="xl:ml-20 xl:w-5/12 lg:w-5/12 md:w-8/12 mb-12 md:mb-0">
             <form>
               <div className="flex items-center my-2 before:flex-1 before:border-t before:border-black before:mt-0.5 after:flex-1 after:border-t after:border-black after:mt-0.5"></div>
-              {login_state ? <Login /> : <Sign_up />}
-            </form>
-            <div className="flex justify-between items-center mb-6">
-              <div className="form-group form-check">
-                <input
-                  type="checkbox"
-                  className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
-                  id="exampleCheck2"
-                />
-                <label
-                  className="form-check-label inline-block text-gray-800"
-                  htmlFor="exampleCheck2"
-                >
-                  Remember me
-                </label>
+              {login_state ? (
+                <div className="">
+                  <div className="Sign_in grid text-2xl text-black text-center mb-2">
+                    Sign In
+                  </div>
+                  {/* Email input */}
+                  <div className="mb-6">
+                    <input
+                      ref={usernameRef}
+                      type="email"
+                      className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                      name="username"
+                      id="username"
+                      placeholder="Email address"
+                    />
+                  </div>
+                  {/* Password input */}
+                  <div className="mb-6">
+                    <input
+                      ref={passwordRef}
+                      type="password"
+                      name="password"
+                      className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                      id="password"
+                      placeholder="Password"
+                    />
+                  </div>
+                </div>
+              ) : (
+                <Sign_up />
+              )}
+              <div className="flex justify-between items-center mb-6">
+                <div className="form-group form-check">
+                  <input
+                    type="checkbox"
+                    className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+                    id="exampleCheck2"
+                  />
+                  <label
+                    className="form-check-label inline-block text-gray-800"
+                    htmlFor="exampleCheck2"
+                  >
+                    Remember me
+                  </label>
+                </div>
+                <a href="#!" className="text-gray-800">
+                  Forgot password?
+                </a>
               </div>
-              <a href="#!" className="text-gray-800">
-                Forgot password?
-              </a>
-            </div>
-            <div className="text-center lg:text-left">
-              <button
-                type="button"
-                className="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
-                onClick={change_login_state}
-              >
-                {login_state ? "Login" : "Sign UP"}{" "}
-              </button>
-              <p className="text-sm font-semibold mt-2 pt-1 mb-0">
-                {login_state
-                  ? "Don't have an account"
-                  : "Already have an account"}{" "}
-                <a
-                  href="#!"
-                  className="text-red-600 hover:text-red-700 focus:text-red-700 transition duration-200 ease-in-out"
+              <div className="text-center lg:text-left">
+                <button
+                  type="submit"
+                  className="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
                   onClick={change_login_state}
                 >
-                  {login_state ? "Register" : "Sign In"}{" "}
-                </a>
-              </p>
-            </div>
+                  {login_state ? "Login" : "Sign UP"}{" "}
+                </button>
+                <p className="text-sm font-semibold mt-2 pt-1 mb-0">
+                  {login_state
+                    ? "Don't have an account"
+                    : "Already have an account"}{" "}
+                  <a
+                    href="#!"
+                    className="text-red-600 hover:text-red-700 focus:text-red-700 transition duration-200 ease-in-out"
+                    onClick={change_login_state}
+                  >
+                    {login_state ? "Register" : "Sign In"}{" "}
+                  </a>
+                </p>
+              </div>
+            </form>
           </div>
         </div>
       </div>
