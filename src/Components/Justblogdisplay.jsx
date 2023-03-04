af9974ce9da2b7cfaaaf3d183c7dc291d8d6cd71
‘tahaha27’ <M.TahaMahmood27@gmail.com>
2023-03-05 00:06:25 +0500

    some UI changes

--- src/Components/Justblogdisplay.jsx
+++ src/Components/Justblogdisplay.jsx
@@ -9,8 +9,12 @@ const Justblogdisplay = (props) => {
         alt="blog_picture"
       />
       <div className="mt-2">
-        <div className="text-2xl">{props.xy.title}</div>
-        <div className="text-sm">{props.xy.description.substring(0, 150)}</div>
+        <div className="text-2xl h-16 overflow-hidden">{props.xy.title}</div>
+        <hr className="border border-[#d9d9d9] mt-2"></hr>
+        <div className="text-sm h-[60px]  overflow-hidden">
+          {props.xy.description.substring(0, 200)}
+        </div>
+        <hr className="border border-[#d9d9d9] mt-2"></hr>
       </div>
       <div className="flex justify-start h-auto mt-2 ">
         <div className="pt-1">
